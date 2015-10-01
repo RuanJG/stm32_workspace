15c020964086a8afc893a1f5b64af7b58324959d
RuanJG <ruan.mails@gmail.com>
2015-10-01 15:17:43 +0800

    Ruan: modify the  path struct

--- tools/build.sh
+++ tools/build.sh
@@ -1,2 +1,4 @@
+#make -C ./libopencm3 clean
+#make -C ./libopencm3
 make clean
 make hex V=1
--- tools/burn.sh
+++ tools/burn.sh
@@ -1,5 +1,9 @@
-if [ $1'x' == 'x' ];then
-	echo no filename 
+app=$(find ./ -name *.hex)
+if [ $1'x' != 'x' ];then
+	app=$1
+fi
+if [ $app'x' == 'x' ];then
+	echo no *.hex find or input 
 	exit 1
 fi
-sudo ./tools/stm32flash -w $1 -v -g 0x0 /dev/ttyUSB0
+sudo ./tools/stm32flash -w $app -v -g 0x0 /dev/ttyUSB0
